757d6dfd7600891520ed024ff5c38e1dc8bd99ac
Anthony Rosario <anthonymrosario225@gmail.com>
2021-05-17 21:02:03 -0700

    laid out quote file structure

--- src/components/app/quote/Quote.jsx
+++ src/components/app/quote/Quote.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Quote(props) {
+function Quote({ simpQuotes }) {
+  const { character, quote, image } = simpQuotes;
   return (
     <div>
-      
+      <img alt={character} src={image} />
+      <h5>{quote}</h5>
     </div>
   )
 }
 
 Quote.propTypes = {
-
+  simpQuotes: PropTypes.shape({
+    character: PropTypes.string,
+    quote: PropTypes.string,
+    image: PropTypes.string,
+  }).isRequired
 }
 
-export default Quote
-
+export default Quote;
